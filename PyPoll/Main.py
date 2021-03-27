0a465a9712d15d111c6e242010588e66ac9ab91f
Amanda Troxel <amandatroxel2@gmail.com>
2021-03-27 17:46:51 -0600

    update

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -13,6 +13,15 @@ with open(csvpath, 'r') as csvfile:
 
     for row in csvreader:
         Total_Votes = Total_Votes + 1
+        Candidate_Name = row[2]
+
+        if Candidate_Name not in Candidates:
+            Candidates.append(row[2])
+            C_index = Candidates.index(row[2])
+            Vote_Count.append(1)
+        else:
+            C_index = int(Candidates.index(row[2]))
+            Vote_Count[C_index] = Vote_Count[C_index] + 1
 
 
 
