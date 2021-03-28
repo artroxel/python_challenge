d8ab1c4db069190c1e0d19b1bbc4049c38fe8884
Amanda Troxel <amandatroxel2@gmail.com>
2021-03-27 20:34:23 -0600

    pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,38 +15,35 @@ with open(csvpath, 'r') as csvfile:
     Avg_Chg_Rev = 0
     Increase_Prof = 0
     Decrease_Loss = 0
-    Prof_Loss = int(row[1])
 
     for row in csvreader:
-         Total_Months = Total_Months + 1
-         Total_Rev = Total_Rev + Prof_Loss
+        Total_Months = Total_Months + 1
+        Total_Rev = Total_Rev + int(row[1])
 
-        if Prof_Loss - Avg_Chg_Rev < Decrease_Loss:
-            Decrease_Loss = Prof_Loss - Avg_Chg_Rev
+        if int(row[1]) - Avg_Chg_Rev < Decrease_Loss:
+            Decrease_Loss = int(row[1]) - Avg_Chg_Rev
             Decrease_Month = row[0]
-        elif Prof_Loss - Avg_Chg_Rev > Increase_Prof:
-            Increase_Prof = Prof_Loss - Avg_Chg_Rev
+        elif int(row[1]) - Avg_Chg_Rev > Increase_Prof:
+            Increase_Prof = int(row[1]) - Avg_Chg_Rev
             Increase_Month = row[0]
-            Diff = Diff + Prof_Loss   
 
-    Avg_Chg_Rev = Prof_Loss/len(Prof_Loss)
+    Diff = Diff + int(row[1])
+    Avg_Chg_Rev = Diff/Total_Rev
       
-        
+print('Financial Analysis\n')
+print('-----------------\n')
+print('Total Months:' + str(Total_Months)+ '\n')
+print('Total: $'+ str(Total_Rev) + '\n')
+print('Average Change: $' + str(round((Diff/(Total_Months - 1)),2)) + '\n')
+print('Greatest Increase in Profits: '+ Increase_Month + ' ($'+ str(Increase_Prof) + ')\n')
+print('Greatest Decrease in Profits: ' + Decrease_Month + ' ($' + str(Decrease_Loss) + ')\n')
 
-
-    print('Financial Analysis \n')
-    print('----------------------- \n')
-    print('Total Months:' + str(Total_Months) + '\n')
-    print('Average Change: $' + str((Change/(Total_Months - 1)) + '\n')
-    print('Greatest Increase in Profits: '+ Increase_Month + ' ($'+ str(Increase_Prof) + ')\n')
-    print('Greatest Decrease in Profits: ' + Decrease_Month + ' ($' + str(Decrease_Loss) + ')\n')
-
-    output_path = os.path.join('Analysis','Budget_Data.txt')
+output_path = os.path.join('Analysis','Budget_Data.txt')
 with open(output_path,'w', newline = '') as datafile:
     datafile.write('Financial Analysis \n')
     datafile.write('----------------------- \n')
     datafile.write('Total Months:' + str(Total_Months) + '\n')
-    datafile.write('Average Change: $' + str((Change/(Total_Months - 1)) + '\n')
+    datafile.write('Average Change: $' + str(round((Diff/(Total_Months - 1)),2)) + '\n')
     datafile.write('Greatest Increase in Profits: '+ Increase_Month + ' ($'+ str(Increase_Prof) + ')\n')
     datafile.write('Greatest Decrease in Profits: ' + Decrease_Month + ' ($' + str(Decrease_Loss) + ')\n')
 
